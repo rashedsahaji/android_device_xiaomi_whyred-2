09ffdff854b63bf8a7813a3bd7d8824e391b93a8
celtare21 <celtare21@gmail.com>
2019-01-18 09:56:47 +0000

    props: Optimize ART props Change-Id: Ib384409127c09e611d1e875b6827e744298b8499 Signed-off-by: celtare21 <celtare21@gmail.com> Signed-off-by: DennySPB <dennyspb@gmail.com> Signed-off-by: rashedsahaji <asifrashed2@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -76,3 +76,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.vendor.shutdown.waittime=500
+
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.image-dex2oat-filter=speed \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=8 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-threads=8
