aab127103f2e74283be0881d9eb3ac64127c388b
srfarias <erikofarias@gmail.com>
2019-02-19 13:34:13 -0300

    whyred: build missing camera.device impl
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- device.mk
+++ device.mk
@@ -151,7 +151,11 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    libxml2
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    camera.device@3.3-impl \
+    libxml2 \
+    Snap
 
 # CNE
 PRODUCT_PACKAGES += \
